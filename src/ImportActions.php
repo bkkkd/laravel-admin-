7935ab645d977423124f11d20d23724758b3200f
Tim <bkkkd@163.com>
2020-12-10 06:12:57 +0800

    增加导入方法的action

---
+++ src/ImportActions.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace bkkkd\LaravelAdminExcel;
+
+
+use App\Models\ChannelPrice;
+use App\Models\Country;
+use Carbon\Carbon;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Widgets\Form as WidgetsForm;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator as MValidator;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+
+trait ImportActions
+{
+
+    public function importForm(Content $content,Request $request){
+        $form = new WidgetsForm();
+        $form->action('import');
+        $form->file('import')->rules('required')
+            ->options(['initialPreviewAsData'=>false,'overwriteInitial'=>false,'showPreview'=>false]);
+
+        return $content->header('导入数据')
+            ->body($form);
+    }
+    public function importPost(){
+
+        $data = $this->validate(Request(), ['import'=>'required']);
+        $sheet = IOFactory::load($data['import']->getRealpath());
+        return $this->importSaving($sheet);
+    }
+    abstract function importSaving(Spreadsheet $sheet):Content;
+}

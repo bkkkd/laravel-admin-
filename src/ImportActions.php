128518103206825c09f1ac04678900e59a63c14f
Tim <bkkkd@163.com>
2020-12-11 21:22:00 +0800

    调整

--- src/ImportActions.php
+++ src/ImportActions.php
@@ -5,7 +5,9 @@ namespace bkkkd\LaravelAdminExcel;
 
 
 
+use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
+use Encore\Admin\Widgets\Box;
 use Encore\Admin\Widgets\Form as WidgetsForm;
 use Illuminate\Http\Request;
 use PhpOffice\PhpSpreadsheet\IOFactory;
@@ -17,8 +19,9 @@ trait ImportActions
     public function importForm(Content $content,Request $request){
         $form = new WidgetsForm();
         $form->action('import');
-        $form->file('import')->rules('required')
+        $file = $form->file('import')->rules('required')
             ->options(['initialPreviewAsData'=>false,'overwriteInitial'=>false,'showPreview'=>false]);
+        $file->help("<a href='importTpl'>模板</a>");
 
         return $content->header('导入数据')
             ->body($form);

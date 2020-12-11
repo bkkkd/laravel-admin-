44c950d707c38cbdad53a552a7b46d41d7ad4784
Tim <bkkkd@163.com>
2020-12-11 21:38:50 +0800

    调整

--- src/ImportActions.php
+++ src/ImportActions.php
@@ -4,33 +4,53 @@
 namespace bkkkd\LaravelAdminExcel;
 
 
-
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Widgets\Box;
 use Encore\Admin\Widgets\Form as WidgetsForm;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 trait ImportActions
 {
 
-    public function importForm(Content $content,Request $request){
+    public function importForm(Content $content, Request $request)
+    {
         $form = new WidgetsForm();
         $form->action('import');
         $file = $form->file('import')->rules('required')
-            ->options(['initialPreviewAsData'=>false,'overwriteInitial'=>false,'showPreview'=>false]);
-        $file->help("<a href='importTpl'>模板</a>");
+            ->options(['initialPreviewAsData' => false, 'overwriteInitial' => false, 'showPreview' => false]);
+        if($this->importTplResponse()){
+            $file->help("<a href='importTpl'>模板</a>");
+        }
 
         return $content->header('导入数据')
             ->body($form);
     }
-    public function importPost(){
 
-        $data = $this->validate(Request(), ['import'=>'required']);
+    public function importPost()
+    {
+
+        $data = $this->validate(Request(), ['import' => 'required']);
         $sheet = IOFactory::load($data['import']->getRealpath());
         return $this->importSaving($sheet);
     }
+
+    public function importTpl()
+    {
+        $response = $this->importTplResponse();
+        if (empty($response)) {
+            $response = new Response();
+            $response->setStatusCode(404);
+            return $response;
+        }
+        return $response;
+
+    }
+
     abstract protected function importSaving(Spreadsheet $sheet);
+
+    abstract protected function importTplResponse():Response;
 }
